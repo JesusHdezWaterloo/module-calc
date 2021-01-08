1289cde095b34e0e0efda2e5218d1f4f9d833138
Unknown <unknown@example.com>
2021-01-08 17:14:19 -0500

    Reestructurado por clases

--- src/main/java/com/root101/module/util/calc/CalcSwingModule.java
+++ src/main/java/com/root101/module/util/calc/CalcSwingModule.java
@@ -1,14 +1,9 @@
 package com.root101.module.util.calc;
 
-import com.root101.clean.core.app.services.ExceptionHandler;
 import com.root101.clean.swing.app.AbstractSwingApplication;
 import com.root101.clean.swing.app.DefaultAbstractSwingMainModule;
 import com.root101.clean.swing.app.dashboard.DashBoardSimple;
 import com.root101.clean.swing.app.dashboard.DashboardConstants;
-import com.root101.swing.derivable_icons.IconSVG;
-import java.awt.event.ActionEvent;
-import java.io.IOException;
-import javax.swing.AbstractAction;
 
 public class CalcSwingModule extends DefaultAbstractSwingMainModule {
 
@@ -28,17 +23,7 @@ public class CalcSwingModule extends DefaultAbstractSwingMainModule {
     private void registerCalc(AbstractSwingApplication app) {
         DashBoardSimple dash = app.rootView().dashboard();
 
-        IconSVG CALC = new IconSVG(getClass().getResourceAsStream("/calc.svg"));
-        dash.addKeyValue(DashboardConstants.UP_ELEMENT, new AbstractAction("Calculadora", CALC) {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Runtime.getRuntime().exec("calc");
-                } catch (IOException ex) {
-                    ExceptionHandler.handleException(ex);
-                }
-            }
-        });
+        dash.addKeyValue(DashboardConstants.UP_ELEMENT, CalcAction.from());
     }
 
 }
